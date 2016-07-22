6e13b869c34ec7a3e306eeb51899a8cc09179550
zacscodingclub <zbaston@gmail.com>
2016-07-21 20:50:47 -0500

    Done.

--- tests/MainController.spec.js
+++ tests/MainController.spec.js
@@ -1,5 +1,16 @@
-describe('example test', function() {
-    it('should be true', function() {
-        expect('foo').toBe('foo');
+describe('MainController', function() {
+    beforeEach(module('app'));
+
+    var $controller;
+
+    beforeEach(inject(function(_$controller_) {
+      $controller = _$controller_;
+    }));
+
+    it('should have Steve Jobs name' , function() {
+      var $scope = {};
+      var controller = $controller('MainController', { $scope: $scope });
+
+      expect(controller.name).toEqual('Steve Jobs');
     });
 });
